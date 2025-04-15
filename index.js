cf6a13ba44fb50ce57cbef40d4321fc1425ef794
Diego Silveira <diego.silva@privacytools.com.br>
2025-04-15 19:40:28 -0300

    Alteração no index para que as API's sejam encontradas fora do localhost

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ require('./APIs/basic-auth')(app);
 require('./APIs/bearer-auth')(app);
 require('./APIs/oauth2-auth')(app);
 
-app.listen(6666, () => {
-    console.log('API rodando em http://localhost:6666');
+app.listen(6666, '0.0.0.0', () => {
+    console.log('Servidor rodando na porta 6666 em todas as interfaces');
 });
